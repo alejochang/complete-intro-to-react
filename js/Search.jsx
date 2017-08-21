96fe01fd9963602b5343356f810eb19e6c2f220e
alejochang <alejochang@gmail.com>
2017-08-21 10:18:43 -0400

    React Lifecycle Methods

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,31 +1,37 @@
+// @flow
+
 import React, { Component } from 'react';
 import ShowCard from './ShowCard';
-import preload from '../data.json';
+import Header from './Header';
 
 class Search extends Component {
   state = {
-    searchTerm: 'game'
+    searchTerm: '',
   };
-  handleSearchTermChange = event => {
+  props: {
+    shows: Show[],
+  };
+
+  handleSearchTermChange = (
+    event: SyntheticKeyboardEvent & { target: HTMLInputElement },
+  ) => {
     this.setState({ searchTerm: event.target.value });
   };
   render() {
     return (
       <div className="search">
-        <header>
-          <h1>svideo</h1>
-          <input
-            onChange={this.handleSearchTermChange}
-            value={this.state.searchTerm}
-            type="text"
-            placeholder="Search"
-          />
-        </header>
+        <Header
+          showSearch
+          searchTerm={this.state.searchTerm}
+          handleSearchTermChange={this.handleSearchTermChange}
+        />
         <div>
-          {preload.shows
+          {this.props.shows
             .filter(
               show =>
-                `${show.title} ${show.description}`.toUpperCase().indexOf(this.state.searchTerm.toUpperCase()) >= 0
+                `${show.title} ${show.description}`
+                  .toUpperCase()
+                  .indexOf(this.state.searchTerm.toUpperCase()) >= 0,
             )
             .map(show => <ShowCard key={show.imdbID} {...show} />)}
         </div>
